aed40d40e476fffd2a41b259515b88eb436f8e7f
Waxiest15 <waxiest15@gmail.com>
2023-05-04 17:53:19 -0600

    Details Update

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { View, useWindowDimensions, Text, Image } from "react-native";
-import { Box, Center } from "native-base";
+import { View, useWindowDimensions, Text, Image, Button } from "react-native";
+import { Box, Center, HStack, VStack } from "native-base";
 import { TabView, SceneMap } from "react-native-tab-view";
 
 import color from "../utils/Colors";
@@ -8,14 +8,54 @@ import BackButton from "../utils/components/BackButton";
 
 var user = require("../resources/images/UserExample_1.png");
 
-const DataResume = () =>{
-  return(
-    <Box>
-      <Text style={{fontWeight: 'bold'}}>Hola Mau </Text>
-      <Text style={{color: 'gray'}}>Pendejo </Text>
+const Days = (props) => {
+  return (
+    <Box
+      w={10}
+      h={10}
+      backgroundColor={"gray.200"}
+      borderWidth={1}
+      borderRadius={5}
+    >
+      <Center py={"2"}>
+        <Text>{props.day}</Text>
+      </Center>
     </Box>
   );
-}
+};
+
+const Week = (props) => {
+  return (
+    <Center mt={3}>
+      <HStack space={3}>
+        <Days day="Mo" />
+        <Days day="Tu" />
+        <Days day="We" />
+        <Days day="Th" />
+        <Days day="Fr" />
+        <Days day="Sa" />
+        <Days day="Su" />
+      </HStack>
+    </Center>
+  );
+};
+
+const QR = () => {
+  return (
+    <Center mt={5} borderWidth={1} w={"80%"} h={"full"}>
+      <Text>Aqui va el QR jaja</Text>
+    </Center>
+  );
+};
+
+const DataResume = (props) => {
+  return (
+    <Box mt={2}>
+      <Text style={{ fontWeight: "bold" }}>{props.data} </Text>
+      <Text style={{ color: "gray" }}>{props.type} </Text>
+    </Box>
+  );
+};
 
 const MainRoute = () => (
   <View style={{ flex: 1, backgroundColor: color.BackgroundApp }}>
@@ -24,17 +64,43 @@ const MainRoute = () => (
     </Box>
     <Box>
       <Center backgroundColor={"amber.200"}>
-        <Image
-          source={require("../resources/images/UserExample_1.png")}
-        />
+        <Image source={require("../resources/images/UserExample_1.png")} />
+        <Box borderWidth={1} borderRadius={10} height={"50"} width={"50"}></Box>
         <Text>Marco Antonio</Text>
       </Center>
-      <Box>
-        <Text>Information</Text>
+
+      <Box mt={3} borderWidth={1} borderRadius={10} mx={10} p={2} shadow={"5"}>
         <Center>
-      <DataResume/>
+          <Text>Information</Text>
+          <HStack space={2}>
+            <VStack justifyContent={"space-between"}>
+              <DataResume data="XDD" type="Age" />
+              <DataResume data=":D" type={"Health\nCondition"} />
+            </VStack>
+
+            <VStack justifyContent={"space-between"}>
+              <DataResume data="38°" type="Temperature" />
+              <DataResume data="140" type="Glucose" />
+            </VStack>
+
+            <VStack justifyContent={"space-between"}>
+              <DataResume data="120/80" type="Pressure" />
+              <DataResume data="3" type="Bed" />
+            </VStack>
+          </HStack>
         </Center>
       </Box>
+      <Week />
+      <Center>
+        <QR />
+      </Center>
+
+      <Center mt={5} justifyContent={"space-around"}>
+        <VStack space={2}>
+          <Button title={"Print QR"} />
+          <Button title={"Print full patient information"} />
+        </VStack>
+      </Center>
     </Box>
   </View>
 );
@@ -60,7 +126,7 @@ export default function TabViewExample() {
   ]);
 
   return (
-    <TabView
+    <TabView 
       navigationState={{ index, routes }}
       renderScene={renderScene}
       onIndexChange={setIndex}
